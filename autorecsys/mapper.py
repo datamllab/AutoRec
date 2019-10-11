fdb743c3e492e37a1c662ebc33eeba2bd2cbe19e
han <han@tamu.edu>
2019-10-11 02:17:09 +0000

    update config

--- autorecsys/mapper.py
+++ autorecsys/mapper.py
@@ -2,18 +2,32 @@ import tensorflow as tf
 
 
 class baseMapper( tf.keras.layers.Layer ):
-    def __init__(self):
+    def __init__(self, **kwarg):
         super(baseMapper, self).__init__()
+        # self.name = name
         pass
 
 
     def call(self, x):
-        pass
+        return x
 
 
 
 
 class LatentFactorMapper( baseMapper ):
+    '''
+    latent factor mapper for cateory datas
+    '''
+    def __init__(self, id_num, embedding_dim):
+        super( LatentFactorMapper, self ).__init__()
+        self.user_embedding = tf.keras.layers.Embedding( id_num, embedding_dim )
+
+    def call(self, x):
+        x = self.user_embedding( x )
+        return x
+
+
+class CompactLatentFactorMapper( baseMapper ):
     '''
     latent factor mapper for cateory datas
     '''
