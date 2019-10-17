bbf9a032787d8cc9a5d83ed8e9e8301f7cd96941
song3134 <qq@datalab3.engr.tamu.edu>
2019-10-17 14:23:05 -0700

    remove xml

--- autorecsys/mapper.py
+++ autorecsys/mapper.py
@@ -2,9 +2,7 @@ from abc import ABCMeta, abstractmethod
 import tensorflow as tf
 
 
-
-class BaseMapper( tf.keras.Model, metaclass=ABCMeta ):
-# class BaseMapper( tf.keras.layers.Layer, metaclass=ABCMeta ):
+class BaseMapper(tf.keras.Model, metaclass=ABCMeta):
     def __init__(self, **kwarg):
         super(BaseMapper, self).__init__()
         # self.name = name
@@ -14,19 +12,18 @@ class BaseMapper( tf.keras.Model, metaclass=ABCMeta ):
         """call model."""
 
 
-
-class LatentFactorMapper( BaseMapper ):
-    '''
+class LatentFactorMapper(BaseMapper):
+    """
     latent factor mapper for cateory datas
-    '''
+    """
+
     def __init__(self, id_num, embedding_dim):
-        super( LatentFactorMapper, self ).__init__()
-        self.user_embedding = tf.keras.layers.Embedding( id_num, embedding_dim )
+        super(LatentFactorMapper, self).__init__()
+        self.user_embedding = tf.keras.layers.Embedding(id_num, embedding_dim)
 
     def call(self, x):
-        x = self.user_embedding( x )
+        x = self.user_embedding(x)
         return x
-
 
 # class CompactLatentFactorMapper( BaseMapper ):
 #     '''
--- autorecsys/optimizer.py
+++ autorecsys/optimizer.py
@@ -2,31 +2,26 @@ import tensorflow as tf
 from abc import ABCMeta, abstractmethod
 
 
-class BaseOptimizer( tf.keras.Model, metaclass=ABCMeta  ):
+class BaseOptimizer(tf.keras.Model, metaclass=ABCMeta):
     def __init__(self):
         super(BaseOptimizer, self).__init__()
-
 
     @abstractmethod
     def call(self, x):
         """call model."""
 
 
-
-
-class RatingPredictionOptimizer( BaseOptimizer ):
-    '''
+class RatingPredictionOptimizer(BaseOptimizer):
+    """
     latent factor mapper for cateory datas
-    '''
+    """
+
     def __init__(self):
-        super( RatingPredictionOptimizer, self ).__init__()
-        # self.user_embed = user_embed
-        # self.item_embed = item_embed
-        self.dense_layer = tf.keras.layers.Dense( 1 )
+        super(RatingPredictionOptimizer, self).__init__()
+        self.dense_layer = tf.keras.layers.Dense(1)
 
     def call(self, embeds):
-        x = tf.concat( embeds, axis=1 )
-        x = self.dense_layer( x )
-        x = tf.reshape( x, [-1] )
-        # x = tf.reduce_sum( x, axis = 1 )
+        x = tf.concat(embeds, axis=1)
+        x = self.dense_layer(x)
+        x = tf.reshape(x, [-1])
         return x
