4d9a869d7e4e507cbe590b3d5f827534456f8568
song3134 <qq@datalab3.engr.tamu.edu>
2019-10-17 14:23:05 -0700

    remove xml

--- autorecsys/interaction.py
+++ autorecsys/interaction.py
@@ -2,64 +2,73 @@ import tensorflow as tf
 from abc import ABCMeta, abstractmethod
 
 
-class BaseInteraction( tf.keras.Model, metaclass=ABCMeta ):
+class BaseInteraction(tf.keras.Model, metaclass=ABCMeta):
     def __init__(self):
         super(BaseInteraction, self).__init__()
         pass
 
     @abstractmethod
-    def call(self, x):
-        """call model."""
+    def call(self):
+        raise NotImplementedError
 
 
-
-class InnerProductInteraction( BaseInteraction ):
+class InnerProductInteraction(BaseInteraction):
     '''
-    latent factor mapper for cateory datas
+    latent factor mapper for category datas
     '''
+
     def __init__(self):
-        super( InnerProductInteraction, self ).__init__()
+        super(InnerProductInteraction, self).__init__()
 
     def call(self, embed1, embed2):
+        """
+
+        Args:
+            embed1:
+            embed2:
+
+        Returns:
+
+        """
         x = embed1 * embed2
         return x
 
 
-class MLPInteraction_legency( BaseInteraction ):
-    '''
-    latent factor mapper for cateory datas
-    '''
-    def __init__(self):
-        super( MLPInteraction, self ).__init__()
-        self.dense_layers = []
-        self.dense_layers.append( tf.keras.layers.Dense(128) )
-        self.dense_layers.append( tf.keras.layers.Dense( 128 ) )
-        self.dense_layers.append( tf.keras.layers.Dense( 128 ) )
-        self.dense_layers.append( tf.keras.layers.Dense( 64 ) )
+class MLPInteraction_legency(BaseInteraction):
+    """
+    latent factor mapper for categorical features
+    """
 
-    def call(self, embeds ):
-        x = tf.concat( embeds, axis = 1 )
+    def __init__(self):
+        super(MLPInteraction, self).__init__()
+        self.dense_layers = []
+        self.dense_layers.append(tf.keras.layers.Dense(128))
+        self.dense_layers.append(tf.keras.layers.Dense(128))
+        self.dense_layers.append(tf.keras.layers.Dense(128))
+        self.dense_layers.append(tf.keras.layers.Dense(64))
+
+    def call(self, embeds):
+        x = tf.concat(embeds, axis=1)
         for layer in self.dense_layers:
-            x = layer( x )
+            x = layer(x)
         return x
 
 
-
-class MLPInteraction( BaseInteraction ):
-    '''
+class MLPInteraction(BaseInteraction):
+    """
     latent factor mapper for cateory datas
-    '''
-    def __init__(self):
-        super( MLPInteraction, self ).__init__(  )
-        self.dense_layers = []
-        self.dense_layers.append( tf.keras.layers.Dense(128) )
-        self.dense_layers.append( tf.keras.layers.Dense( 128 ) )
-        self.dense_layers.append( tf.keras.layers.Dense( 128 ) )
-        self.dense_layers.append( tf.keras.layers.Dense( 64 ) )
+    """
 
+    def __init__(self):
+        super(MLPInteraction, self).__init__()
+        self.dense_layers = []
+        self.dense_layers.append(tf.keras.layers.Dense(128))
+        self.dense_layers.append(tf.keras.layers.Dense(128))
+        self.dense_layers.append(tf.keras.layers.Dense(128))
+        self.dense_layers.append(tf.keras.layers.Dense(64))
 
     def call(self, embeds):
-        x = tf.concat( embeds , axis = 1 )
+        x = tf.concat(embeds, axis=1)
         for layer in self.dense_layers:
-            x = layer( x )
+            x = layer(x)
         return x
--- autorecsys/recommender.py
+++ autorecsys/recommender.py
@@ -4,10 +4,9 @@ from autorecsys.interaction import *
 from autorecsys.optimizer import *
 
 
-
-class recommender( tf.keras.Model ):
-    def __init__( self, mapper_list,  interaction_list, optimizer_list):
-        super( recommender, self ).__init__()
+class recommender(tf.keras.Model):
+    def __init__(self, mapper_list, interaction_list, optimizer_list):
+        super(recommender, self).__init__()
         self.mapper_list = mapper_list
         self.interaction_list = {}
         self.optimizer_list = {}
@@ -18,32 +17,30 @@ class recommender( tf.keras.Model ):
         # self.user_latentfactorMapper = LatentFactorMapper( userID_max, embedding_dim )
         # self.item_latentfactorMapper = LatentFactorMapper( itemID_max, embedding_dim )
 
-        self.user_latentfactorMapper = self.mapper_list[ "user_id" ]
-        self.item_latentfactorMapper = self.mapper_list[ "item_id" ]
+        self.user_latentfactorMapper = self.mapper_list["user_id"]
+        self.item_latentfactorMapper = self.mapper_list["item_id"]
 
         # self.interaction = InnerProductInteraction()
         self.interaction = MLPInteraction()
-
 
         self.optimizer = RatingPredictionOptimizer()
 
         # self.user_embedding = tf.keras.layers.Embedding( userID_max, embedding_dim )
         # self.item_embedding = tf.keras.layers.Embedding( itemID_max, embedding_dim )
 
-    def call( self, userID, ItemID ):
-        userID_embedding = self.user_latentfactorMapper( userID )
-        itemID_embedding = self.item_latentfactorMapper( ItemID )
+    def call(self, userID, ItemID):
+        userID_embedding = self.user_latentfactorMapper(userID)
+        itemID_embedding = self.item_latentfactorMapper(ItemID)
         # print( "embedding", userID_embedding, itemID_embedding )
 
-        y_pred = self.interaction( userID_embedding, itemID_embedding )
+        y_pred = self.interaction(userID_embedding, itemID_embedding)
         # print( "y_pred",y_pred )
 
-        y_pred = self.optimizer( y_pred )
+        y_pred = self.optimizer(y_pred)
         # y_pred = tf.reduce_sum( y_pred, axis = 1 )
         # print( "y_pred_sum", y_pred )
 
         return y_pred
-
 
 # class MF( tf.keras.Model ):
 #     def __init__( self, userID_max, itemID_max, embedding_dim ):
@@ -69,4 +66,3 @@ class recommender( tf.keras.Model ):
 #         # print( "y_pred_sum", y_pred )
 #
 #         return y_pred
-
