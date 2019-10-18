8811703b320901ebd17bdf25ef0e05fcec008707
song3134 <song_3134@tamu.edu>
2019-10-17 21:00:28 -0700

    finish basic refactor

--- autorecsys/interaction.py
+++ autorecsys/interaction.py
@@ -2,10 +2,29 @@ import tensorflow as tf
 from abc import ABCMeta, abstractmethod
 
 
+def set_interactor_from_config(interactor_name, interactor_config):
+    if interactor_name is None:
+        return None
+    name2interactor = {
+        "MLP": MLPInteraction,
+        "InnerProduct": InnerProductInteraction,
+    }
+    return name2interactor[interactor_name](interactor_config)
+
+
+def build_interactors(interactor_list):
+    interactor_configs = [(k, v) for interactor in interactor_list for k, v in interactor.items()]
+    interactors = [
+        set_interactor_from_config(interactor[0], interactor[1])
+        for interactor in interactor_configs
+    ]
+    return interactors
+
+
 class BaseInteraction(tf.keras.Model, metaclass=ABCMeta):
-    def __init__(self):
-        super(BaseInteraction, self).__init__()
-        pass
+    def __init__(self, config):
+        super(BaseInteraction, self).__init__(config)
+        self.config = config
 
     @abstractmethod
     def call(self):
@@ -13,14 +32,15 @@ class BaseInteraction(tf.keras.Model, metaclass=ABCMeta):
 
 
 class InnerProductInteraction(BaseInteraction):
-    '''
-    latent factor mapper for category datas
-    '''
+    """
+    latent factor interactor for category datas
+    """
 
-    def __init__(self):
-        super(InnerProductInteraction, self).__init__()
+    def __init__(self, config):
+        super(InnerProductInteraction, self).__init__(config)
 
-    def call(self, embed1, embed2):
+    def call(self, embeds):
+        # TODO:
         """
 
         Args:
@@ -30,17 +50,15 @@ class InnerProductInteraction(BaseInteraction):
         Returns:
 
         """
-        x = embed1 * embed2
-        return x
-
+        pass
 
 class MLPInteraction_legency(BaseInteraction):
     """
-    latent factor mapper for categorical features
+    latent factor interactor for categorical features
     """
 
-    def __init__(self):
-        super(MLPInteraction, self).__init__()
+    def __init__(self, config):
+        super(MLPInteraction, self).__init__(config)
         self.dense_layers = []
         self.dense_layers.append(tf.keras.layers.Dense(128))
         self.dense_layers.append(tf.keras.layers.Dense(128))
@@ -48,6 +66,7 @@ class MLPInteraction_legency(BaseInteraction):
         self.dense_layers.append(tf.keras.layers.Dense(64))
 
     def call(self, embeds):
+        # TODO: should revise
         x = tf.concat(embeds, axis=1)
         for layer in self.dense_layers:
             x = layer(x)
@@ -56,11 +75,11 @@ class MLPInteraction_legency(BaseInteraction):
 
 class MLPInteraction(BaseInteraction):
     """
-    latent factor mapper for cateory datas
+    latent factor interactor for cateory datas
     """
 
-    def __init__(self):
-        super(MLPInteraction, self).__init__()
+    def __init__(self, config):
+        super(MLPInteraction, self).__init__(config)
         self.dense_layers = []
         self.dense_layers.append(tf.keras.layers.Dense(128))
         self.dense_layers.append(tf.keras.layers.Dense(128))
@@ -68,7 +87,7 @@ class MLPInteraction(BaseInteraction):
         self.dense_layers.append(tf.keras.layers.Dense(64))
 
     def call(self, embeds):
-        x = tf.concat(embeds, axis=1)
+        x = tf.concat([v for _, v in embeds.items()], axis=1)
         for layer in self.dense_layers:
             x = layer(x)
         return x
