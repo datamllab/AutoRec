aa006fce949dc31c1d9a64ec7cb799936a888280
ahxt <han@tamu.edu>
2019-10-24 11:16:44 -0500

    update by comments

--- autorecsys/trainer.py
+++ autorecsys/trainer.py
@@ -1,11 +1,10 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
-# from autorecsys.utils.config import load_config
 from autorecsys.utils import *
 
 import logging
-# logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.basicConfig(level = logging.DEBUG,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
@@ -27,7 +26,6 @@ def train(model, data, train_config=None):
     avg_loss = []
 
     for step, (user_id, item_id, y) in enumerate(data.take(num_batch)):
-        # logging.debug("test")
         feat_dict = {"user_id": user_id, "item_id": item_id}
         with tf.GradientTape() as tape:
             y_pred = model(feat_dict)
@@ -38,7 +36,7 @@ def train(model, data, train_config=None):
         avg_loss.append(float(loss))
         if train_config["TrainOption"]["logging_config"]["freq"] > 0 \
                 and step % train_config["TrainOption"]["logging_config"]["freq"] == 0:
-            print("Step: {}, avg_loss: {}, loss: {}".format(
+            logging.info("Step: {}, avg_loss: {}, loss: {}".format(
                 step,
                 sum(avg_loss[-1000:]) / min(1000., step + 1),
                 loss
--- autorecsys/utils/__init__.py
+++ autorecsys/utils/__init__.py
@@ -1,5 +1,7 @@
-from .config import *
+from .config import load_config
+from .data import load_dataset
 
 __all__=[
-    "load_config"
+    "load_config",
+    "load_dataset"
 ]
