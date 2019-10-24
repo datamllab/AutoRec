3c826f6d88cb1ff7261c2fc8643dbf13156d9a3a
ahxt <han@tamu.edu>
2019-10-24 02:16:25 -0500

    new files organization

--- autorecsys/trainer.py
+++ autorecsys/trainer.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
-from autorecsys.utils import load_config
+# from autorecsys.utils.config import load_config
+from autorecsys.utils import *
 
 import logging
 # logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -26,21 +27,21 @@ def train(model, data, train_config=None):
     avg_loss = []
 
     for step, (user_id, item_id, y) in enumerate(data.take(num_batch)):
-        logging.debug("test")
-        # feat_dict = {"user_id": user_id, "item_id": item_id}
-        # with tf.GradientTape() as tape:
-        #     y_pred = model(feat_dict)
-        #     loss = tf.keras.losses.MSE(y_pred["rating"], y)
-        # grads = tape.gradient(loss, model.trainable_variables)
-        #
-        # optimizer.apply_gradients(zip(grads, model.trainable_variables))
-        # avg_loss.append(float(loss))
-        # if train_config["TrainOption"]["logging_config"]["freq"] > 0 \
-        #         and step % train_config["TrainOption"]["logging_config"]["freq"] == 0:
-        #     print("Step: {}, avg_loss: {}, loss: {}".format(
-        #         step,
-        #         sum(avg_loss[-1000:]) / min(1000., step + 1),
-        #         loss
-        #         )
-        #     )
+        # logging.debug("test")
+        feat_dict = {"user_id": user_id, "item_id": item_id}
+        with tf.GradientTape() as tape:
+            y_pred = model(feat_dict)
+            loss = tf.keras.losses.MSE(y_pred["rating"], y)
+        grads = tape.gradient(loss, model.trainable_variables)
+
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        avg_loss.append(float(loss))
+        if train_config["TrainOption"]["logging_config"]["freq"] > 0 \
+                and step % train_config["TrainOption"]["logging_config"]["freq"] == 0:
+            print("Step: {}, avg_loss: {}, loss: {}".format(
+                step,
+                sum(avg_loss[-1000:]) / min(1000., step + 1),
+                loss
+                )
+            )
     return model, avg_loss
