4a1b10188a3cb6e74cbe1ec817745e9f62aa228c
song3134 <song_3134@tamu.edu>
2019-10-28 21:53:17 -0700

    add hyperparameter and oracle test

---
+++ autorecsys/utils/common.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+import shutil
+import tensorflow as tf
+
+
+
+def create_directory(path, remove_existing=False):
+    # Create the directory if it doesn't exist.
+    if not os.path.exists(path):
+        os.mkdir(path)
+    # If it does exist, and remove_existing is specified,
+    # the directory will be removed and recreated.
+    elif remove_existing:
+        shutil.rmtree(path)
+        os.mkdir(path)
+
+
+def set_device(device_name):
+    if device_name == "cpu":
+        pass
+    else:
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        print("Available GPUs: {}".format(gpus))
+        assert len(gpus) > 0, "Not enough GPU hardware devices available"
+        gpu_idx = int(device_name[-1])
+        tf.config.experimental.set_visible_devices(gpus[gpu_idx], 'GPU')
