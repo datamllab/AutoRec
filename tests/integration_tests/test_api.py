0ceed33a84de794af8bf4d9b74a80be20f1bfc2a
song3134 <song_3134@tamu.edu>
2019-11-06 15:12:03 -0800

    resolve

---
+++ tests/integration_tests/test_api.py
@@ -0,0 +1,24 @@
+import pytest
+
+from autorecsys.api import Job, AutoSearch
+from autorecsys.pipeline.recommender import Recommender
+
+
+@pytest.fixture(scope='module')
+def tmp_dir(tmpdir_factory):
+    return tmpdir_factory.mktemp('api_test')
+
+
+def test_job_api(tmp_dir):
+    config_filename = "./tests/configs/job_config.yaml"
+    tmp_job = Job(config_filename)
+    tmp_job.run()
+    assert type(tmp_job.recommender) == Recommender
+
+
+def test_search_api(tmp_dir):
+    config_filename = "./tests/configs/random_search_config.yaml"
+    search_api = AutoSearch(config_filename)
+    final_results = search_api.search()
+    # TODO
+    pass
