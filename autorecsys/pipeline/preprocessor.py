8eb0615c4da146702dca8e663eb9c58e75923393
Guacore <thwang1231@gmail.com>
2019-12-15 20:46:10 -0600

    1) Added examples/MLP.py, 2) modify _negative_sampling() under pipeline/preprocessor.py to support sampling with replacement, 3) added 'BinaryCrossentropy' to METRIC under searcher/core/tuner.py, 4) fixed linear interaction for MLP under examples/NeuMF.py, 5) (WIP) fixing bugs in testing _negative_sampling() under tests/pipeline_tests/test_preprocessor.py

--- autorecsys/pipeline/preprocessor.py
+++ autorecsys/pipeline/preprocessor.py
@@ -2,10 +2,15 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from abc import ABCMeta, abstractmethod
+from typing import List
+
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 import time
+import os
+from joblib import Parallel, delayed
+
 
 class BaseProprocessor(metaclass=ABCMeta):
 
@@ -78,6 +83,8 @@ class BasePairWiseProprocessor(BaseProprocessor):
 
 class Movielens1MPreprocessor(BaseRatingPredictionProprocessor):
 
+    used_columns_names: List[str]
+
     def __init__(self, dataset_path):
         super(Movielens1MPreprocessor, self).__init__(dataset_path=dataset_path, )
         self.columns_names = ["user_id", "item_id", "rating", "timestamp"]
@@ -111,64 +118,61 @@ class Movielens1MCTRPreprocessor(BasePointWiseProprocessor):
                                    dtype=self.dtype_dict)
         self.pd_data = self.pd_data[self.used_columns_names]
 
-    def _negative_sampling(self, input_df, num_neg, seed=42):
-        """ Negative sampling with replacement
+    def _negative_sampling(self, input_df, num_neg):
+        """ Add a column of negative item IDs to the input DataFrame
         :param input_df: DataFrame user-item interactions with columns=[user_id, item_id, rating]
         :param num_neg: Integer number of negative interaction to sample per user-item interaction
-        :param seed: Integer seed for random sampling
         :return: DataFrame user-item interactions with columns=[user_id, item_id, rating, neg_item_ids]
         """
-        # Find negative sampling candidate items
+        # Find candidate items for negative sampling
         items = set(input_df['item_id'].unique())
-        user_pos_items = input_df.groupby('user_id')['item_id'].apply(set)
-        user_neg_items = items - user_pos_items
-        input_df['neg_item_ids'] = input_df['user_id'].map(user_neg_items)
+        pos_items = input_df.groupby('user_id')['item_id'].apply(set)
+        neg_items = items - pos_items
+        input_df['neg_item_ids'] = input_df['user_id'].map(neg_items)
 
-        # Perform negative sampling
-        s_time = time.time()
-        input_df['neg_item_ids'] = input_df['neg_item_ids'].agg(
-            lambda x: np.random.RandomState(seed=seed).permutation(list(x))[:num_neg])
-        e_time = time.time()
-        print(f'use {e_time - s_time} seconds')
-        input('\n\ntest ability to pass above code\n\n')
+        # Find negative relations by negative sampling
+        input_df['neg_item_ids'] = input_df['neg_item_ids'].agg(lambda x: np.random.permutation(list(x))[:num_neg])
+
         return input_df
 
     def preprocessing(self, test_size, num_neg, random_state):
-        compact_data = self._negative_sampling(self.pd_data[:self.pd_data.shape[0]//10], num_neg=num_neg, seed=random_state)
-        compact_X = compact_data.iloc[:, self.pd_data.columns != 'rating']
-        compact_y = compact_data['rating'].to_frame()
+        compact_data = self._negative_sampling(self.pd_data, num_neg=num_neg)
+        compact_X = compact_data.loc[:, compact_data.columns != 'rating']
+        compact_y = compact_data[['rating']]
         compact_train_X, compact_val_X, compact_train_y, compact_val_y = train_test_split(compact_X, compact_y,
             test_size=test_size, random_state=random_state)
 
-        def expand(c_X, c_y):
-            c_data = pd.concat([c_X, c_y], axis=1)
+        def expand(unexpanded_X, unexpanded_y):
+            # Extract positive relations
+            unexpanded_data = pd.concat([unexpanded_X, unexpanded_y], axis=1)
+            pos_data = unexpanded_data[['user_id', 'item_id', 'rating']]
+            pos_data['rating'] = 1
 
             # Expand negative relations
-            neg_data = c_data.explode('neg_items').dropna()[['user_id', 'neg_items', 'rating']].rename(
-                columns={'neg_items': 'item_id'})
+            neg_data = unexpanded_data.explode('neg_item_ids').dropna()[['user_id', 'neg_item_ids', 'rating']].rename(
+                columns={'neg_item_ids': 'item_id'})
             neg_data['rating'] = 0
 
-            # Combine with positive relations
-            pos_data = c_data[['user_id', 'item_id', 'rating']]
-            pos_data['rating'] = 1
+            # Combine negative relations with positive relations
             pos_neg_data = pos_data.append(neg_data, ignore_index=True).reset_index(drop=True)
+
+            pos_neg_data["item_id"] = pos_neg_data["item_id"].astype(np.int32)
+            pos_neg_data["user_id"] = pos_neg_data["user_id"].astype(np.int32)
+            pos_neg_data["rating"] = pos_neg_data["rating"].astype(np.int32)
 
             return pos_neg_data
 
         self.pd_data = expand(compact_X, compact_y)
-        self.X = self.pd_data.iloc[:, self.pd_data.columns != 'rating']
-        self.y = self.pd_data['rating'].to_frame()
+        self.X = (self.pd_data.loc[:, self.pd_data.columns != 'rating']).values
+        self.y = self.pd_data['rating'].values
 
         expanded_train_data = expand(compact_train_X, compact_train_y)
-        self.train_X = expanded_train_data.iloc[:, self.pd_data.columns != 'rating']
-        self.train_y = expanded_train_data['rating'].to_frame()
+        self.train_X = (expanded_train_data.loc[:, expanded_train_data.columns != 'rating']).values
+        self.train_y = expanded_train_data['rating'].values
 
-        expanded_val_data = expand(compact_val_X, compact_train_y)
-        self.val_X = expanded_val_data.iloc[:, self.pd_data.columns != 'rating']
-        self.val_y = expanded_val_data['rating'].to_frame()
-
-
-        # Need to set self.pd_data, self.X, self.y, self.train_X, self.val_X, self.train_y, self.val_y
+        expanded_val_data = expand(compact_val_X, compact_val_y)
+        self.val_X = (expanded_val_data.loc[:, expanded_val_data.columns != 'rating']).values
+        self.val_y = expanded_val_data['rating'].values
 
 
 class TabularPreprocessor(BaseProprocessor):
