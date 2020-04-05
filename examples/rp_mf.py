05e146b0eaa1277c8ab14ee24461659d9332ad80
ahxt <han@tamu.edu>
2020-04-04 22:48:17 -0500

    support early stop

--- examples/rp_mf.py
+++ examples/rp_mf.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+
+
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+
+import tensorflow as tf
+# gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
+# for gpu in gpus:
+#     tf.config.experimental.set_memory_growth(gpu, True)
+# import tensorflow as tf
+# physical_devices = tf.config.list_physical_devices('GPU')
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 import logging
 from autorecsys.auto_search import Search
@@ -39,11 +49,13 @@ searcher = Search(model=model,
                   tuner='greedy',  # hyperband, greedy, bayesian
                   tuner_params={"max_trials": 5}
                   )
+
 searcher.search(x=train_X,
                 y=train_y,
                 x_val=val_X,
                 y_val=val_y,
                 objective='val_mse',
-                batch_size=1024)
+                batch_size=1024,
+                epochs=10)
 logger.info('Predicted Ratings: {}'.format(searcher.predict(x=val_X)))
 logger.info('Predicting Accuracy (mse): {}'.format(searcher.evaluate(x=val_X, y_true=val_y)))
