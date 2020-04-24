ca2bbcf3691bfad8298d8a603c1d4bbccdb6b74f
song3134 <song_3134@tamu.edu>
2020-04-24 15:24:44 -0700

    update pipeline docstrings

--- autorecsys/pipeline/utils.py
+++ autorecsys/pipeline/utils.py
@@ -5,14 +5,19 @@ from tensorflow.keras.layers import Layer
 
 
 class Bias(Layer):
+    """A keras layer with only bias term. 
+      (can be understood as MLP layer with zero W matrix)
+      
+    # Attributes:
+        units (int). The units of all layer in the Bias layer.
+    """
+    def __init__(self, units=32):
+          super(Bias, self).__init__()
 
-      def __init__(self, units=32):
-            super(Bias, self).__init__()
+          bias_init = tf.zeros_initializer()
+          self.bias = tf.Variable(initial_value=bias_init(shape=(units,),
+                                                    dtype='float32'),
+                               trainable=True)
 
-            bias_init = tf.zeros_initializer()
-            self.bias = tf.Variable(initial_value=bias_init(shape=(units,),
-                                                      dtype='float32'),
-                                 trainable=True)
-
-      def call(self, inputs):
-            return inputs + self.bias
+    def call(self, inputs):
+          return inputs + self.bias
