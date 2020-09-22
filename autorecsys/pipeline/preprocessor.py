ef2aa7302070916925e837c6921ccaf9aeb0ff36
Guacore <thwang1231@gmail.com>
2020-09-22 00:43:28 -0500

    Added example dataset and updated preprocessor docsting

--- autorecsys/pipeline/preprocessor.py
+++ autorecsys/pipeline/preprocessor.py
@@ -22,7 +22,7 @@ from autorecsys.utils.common import load_pickle, save_pickle
 class BasePreprocessor(metaclass=ABCMeta):
     """ Preprocess data into Pandas DataFrame format.
 
-    Args:
+    # Arguments
         non_csv_path (str): Path to convert the dataset into CSV format.
         csv_path (str): Path to save and load the CSV dataset.
         header (int): Row number to use as column names.
@@ -44,7 +44,7 @@ class BasePreprocessor(metaclass=ABCMeta):
         validate_path (str): Path to the validation set.
         test_path (str): Path to the test set.
 
-    Attributes:
+    # Attributes
         non_csv_path (str): Path to convert the dataset into CSV format.
         csv_path (str): Path to save and load the CSV dataset.
         header (int): Row number to use as column names.
@@ -122,15 +122,15 @@ class BasePreprocessor(metaclass=ABCMeta):
         self.test_path = test_path
 
     def format_dataset(self):
-        """ Convert dataset into CSV format.
+        """ (Optional) Convert dataset into CSV format.
 
-        Note:
-            User is responsible for implementing unsupported data formatters.
+        # Note
+            User should implement this function to convert non-CSV dataset into CSV format.
         """
         raise NotImplementedError
 
     def load_dataset(self):
-        """ Load CSV data into Pandas DataFrame format.
+        """ Load CSV data as a Pandas DataFrame object.
         """
         self.data_df = pd.read_csv(self.csv_path, sep=self.delimiter, header=self.header, names=self.columns, dtype=self.dtype_dict)
         self.data_df.drop(columns=self.ignored_columns, inplace=True)
@@ -139,9 +139,8 @@ class BasePreprocessor(metaclass=ABCMeta):
     def transform_categorical(self):
         """ Transform categorical data.
 
-        Note:
-            Produce fit dictionary for categorical data.
-            Transform categorical data using fit dictionary.
+        # Note
+            Produce fit dictionary for categorical data and transform categorical data using fit dictionary.
         """
         # Step 1: Count categorical occurrences for each column.
         self.fit_dict = {col: self.data_df[col].value_counts().to_dict() for col in self.categorical_columns}
@@ -164,7 +163,7 @@ class BasePreprocessor(metaclass=ABCMeta):
             self.data_df[col] = self.data_df[col].map(self.fit_dict[col])  # set class attribute pd_data
 
     def transform_numerical(self):
-        """ Transform numerical data.
+        """ Transform numerical data using supported data transformation functions.
         """
         # Step 1: Define transformation method.
         def scale_by_log(num):
@@ -179,93 +178,91 @@ class BasePreprocessor(metaclass=ABCMeta):
     def get_hash_size(self):
         """ Get the hash sizes of categorical columns.
 
-        Returns:
-            (list): Integer number of distinctive categories associated with every categorical data columns.
+        # Returns
+            List of integer numbers of categories in each categorical data columns.
         """
         return [len(self.fit_dict[col]) for col in self.categorical_columns]
 
     def get_x(self):
         """ Get the training data columns.
 
-        Returns:
-            (DataFrame): The training data columns.
+        # Returns
+            DataFrame training data columns.
         """
         return self.data_df.drop(columns=self.target_column, inplace=False)
 
     def get_x_numerical(self, x):
-        """ Get the numerical part of the training data columns.
+        """ Get the numerical columns from the input data columns.
 
-        Args:
-            x (DataFrame): The training data columns.
+        # Arguments
+            x (DataFrame): The input data columns.
 
-        Returns:
-            (ndarray): The numerical part of the training data columns.
+        # Returns
+            ndarray numerical columns in the input data columns.
         """
         return x[self.numerical_columns].values
 
     def get_x_categorical(self, x):
-        """ Get the categorical part of the training data columns.
+        """ Get the categorical columns from the input data columns.
 
-        Args:
-            x (DataFrame): The training data columns.
+        # Arguments
+            x (DataFrame): The input data columns.
 
-        Returns:
-            (ndarray): The categorical part of the training data columns.
+        # Returns
+            ndarray categorical columns in the input data columns.
         """
         return x[self.categorical_columns].values
 
     def get_y(self):
-        """ Get the training label column.
+        """ Get the output column.
 
-        Returns:
-            (ndarray): The training label column.
+        # Returns
+            ndarray output column.
         """
         return self.data_df[self.target_column].values
 
     def get_numerical_count(self):
         """ Get the number of numerical columns.
 
-        Returns:
-            (int): The number of numerical columns.
+        # Returns
+            Integer number of numerical columns.
         """
         return len(self.numerical_columns)
 
     def get_categorical_count(self):
         """ Get the number of categorical columns.
 
-        Returns:
-            (int): The number of categorical columns.
+        # Returns
+            Integer number of categorical columns.
         """
         return len(self.categorical_columns)
 
     def split_data(self, x, y, test_percentage):
         """ Split data into the train, validation, and test sets.
 
-        Args:
+        # Arguments
             x (ndarray): (M, N) matrix associated with the numerical and categorical data, where M is the number of rows
                 and N is the number of numerical and categorical columns.
             y (ndarray): (M, 1) matrix associated with the label data, where M is the number of rows.
             test_percentage (float): Percentage of test set.
 
-        Returns:
-            x_train (ndarray): Matrix associated with the part of numerical and categorical data for model training.
-            x_test (ndarray): Matrix associated with the part of numerical and categorical data for model testing.
-            y_train (ndarray): Matrix associated with the part of label data used to train model.
-            y_test (ndarray): Matrix associated with the part of label data for model testing.
+        # Returns
+            4-tuple of ndarray training input data, testing input data, training output data, and testing output data.
         """
         test_size = math.ceil(test_percentage * len(self.data_df.index))
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, stratify=y)
 
         return x_train, x_test, y_train, y_test
 
-    def preprocess(self, **kwargs):
+    def preprocess(self):
         """ Apply all preprocess steps.
 
-        Args:
-            **kwargs: Keyword arguments used to preprocess dataset.
-
-        Note:
+        # Note
             User is responsible for calling the needed data preprocessing functions from here.
+
+        # Returns
+            6-tuple of ndarray training input data, training output data, validation input data, validation output data,
+                testing input data, and testing output data.
         """
         raise NotImplementedError
 
@@ -273,11 +270,11 @@ class BasePreprocessor(metaclass=ABCMeta):
 class AvazuPreprocessor(BasePreprocessor):
     """ Preprocess the Avazu dataset for click-through rate (CTR) prediction.
 
-    Note:
+    # Note:
         To obtain the Avazu dataset, visit: https://www.kaggle.com/c/avazu-ctr-prediction
         The Avazu dataset has 24 data columns: [0] is ignored, [1] is label, and [2-23] are categorical.
 
-    Args:
+    # Arguments
         non_csv_path (str): Path to convert the dataset into CSV format.
         csv_path (str): Path to save and load the CSV dataset.
         header (int): Row number to use as column names.
@@ -357,13 +354,9 @@ class AvazuPreprocessor(BasePreprocessor):
     def preprocess(self):
         """ Apply all preprocessing steps to the Avazu dataset.
 
-        Returns:
-            x_train (ndarray): Training set of the numerical and categorical data.
-            y_train (ndarray): Training set of the label data.
-            x_validate (ndarray): Validation set of the numerical and categorical data.
-            y_validate (ndarray): Validation set of the label data.
-            x_test (ndarray): Testing set of the numerical and categorical data.
-            y_test (ndarray): Testing set of the label data.
+        # Returns
+            6-tuple of ndarray training input data, training output data, validation input data, validation output data,
+                testing input data, and testing output data.
         """
         # Step 1: Load the Avazu dataset.
         self.load_dataset()
@@ -385,11 +378,11 @@ class AvazuPreprocessor(BasePreprocessor):
 class CriteoPreprocessor(BasePreprocessor):
     """ Preprocess the Criteo dataset for click-through rate (CTR) prediction.
 
-    Note:
+    # Note
         To obtain the Criteo dataset, visit: https://www.kaggle.com/c/criteo-display-ad-challenge/
         The Criteo dataset has 40 data columns: [0] is label, [1-13] are numerical, and [14-39] are categorical.
 
-    Args:
+    # Arguments
         non_csv_path (str): Path to convert the dataset into CSV format.
         csv_path (str): Path to save and load the CSV dataset.
         header (int): Row number to use as column names.
@@ -467,17 +460,9 @@ class CriteoPreprocessor(BasePreprocessor):
     def preprocess(self):
         """ Apply all preprocessing steps to the Criteo dataset.
 
-        Args:
-            train_size (float): Ratio of train set w.r.t. the entire Criteo dataset.
-            valid_size (float): Ratio of validation set w.r.t. the entire Criteo dataset.
-
-        Returns:
-            train_x (ndarray): Matrix associated with the part of numerical and categorical data for model training.
-            train_y (ndarray): Matrix associated with the part of label data used to train model.
-            valid_x (ndarray): Matrix associated with the part of numerical and categorical data for model validation.
-            valid_y (ndarray): Matrix associated with the part of label data used to validate model.
-            test_x (ndarray): Matrix associated with the part of numerical and categorical data for model testing.
-            test_y (ndarray): Matrix associated with the part of label data for model testing.
+        # Returns
+            6-tuple of ndarray training input data, training output data, validation input data, validation output data,
+                testing input data, and testing output data.
         """
         # Step 1: Load data for fit and transform categorical data.
         self.load_dataset()
@@ -502,11 +487,11 @@ class CriteoPreprocessor(BasePreprocessor):
 class NetflixPrizePreprocessor(BasePreprocessor):
     """ Preprocess the Netflix dataset for rating prediction.
 
-    Note:
+    # Note
         To obtain the Netflix dataset, visit: https://www.kaggle.com/netflix-inc/netflix-prize-data
         The Netflix dataset has 4 data columns: MovieID, CustomerID, Rating, and Date.
 
-    Args:
+    # Arguments
         non_csv_path (str): Path to convert the dataset into CSV format.
         csv_path (str): Path to save and load the CSV dataset.
         header (int): Row number to use as column names.
@@ -582,6 +567,11 @@ class NetflixPrizePreprocessor(BasePreprocessor):
                          test_path=test_path)
 
     def format_dataset(self):
+        """ Convert the Netflix Prize dataset into CSV format and save it as a new file.
+
+        # Note:
+            This is an example showing the function which converts dataset into the CSV format as provided by user.
+        """
         with open(self.non_csv_path, 'r') as rf, open(self.csv_path, 'w') as wf:
             for line in rf:
                 if ':' in line:
@@ -590,6 +580,12 @@ class NetflixPrizePreprocessor(BasePreprocessor):
                     wf.write(movie + ',' + line)
 
     def preprocess(self):
+        """ Apply all preprocessing steps to the Netflix Prize dataset.
+
+        # Returns
+            6-tuple of ndarray training input data, training output data, validation input data, validation output data,
+                testing input data, and testing output data.
+        """
         # Step 1: Convert Netflix dataset to CSV format.
         self.format_dataset()
 
@@ -613,11 +609,11 @@ class NetflixPrizePreprocessor(BasePreprocessor):
 class MovielensPreprocessor(BasePreprocessor):
     """ Preprocess the Movielens 1M dataset for rating prediction.
 
-    Note:
+    # Note
         To obtain the Movielens 1M dataset, visit: https://grouplens.org/datasets/movielens/
         The Movielens 1M dataset has 4 data columns: UserID, MovieID, Rating, and Timestamp.
 
-    Args:
+    # Arguments
         non_csv_path (str): Path to convert the dataset into CSV format.
         csv_path (str): Path to save and load the CSV dataset.
         header (int): Row number to use as column names.
@@ -639,8 +635,6 @@ class MovielensPreprocessor(BasePreprocessor):
         validate_path (str): Path to the validation set.
         test_path (str): Path to the test set.
     """
-
-    # rp_neumf = 0.7677645683288574
 
     def __init__(self,
                  non_csv_path=None,
@@ -695,6 +689,12 @@ class MovielensPreprocessor(BasePreprocessor):
                          test_path=test_path)
 
     def preprocess(self):
+        """ Apply all preprocessing steps to the Movielens 1M dataset.
+
+        # Returns
+            6-tuple of ndarray training input data, training output data, validation input data, validation output data,
+                testing input data, and testing output data.
+        """
         # Step 1: Load data for fit and transform categorical data.
         self.load_dataset()
 
@@ -706,112 +706,6 @@ class MovielensPreprocessor(BasePreprocessor):
         y = self.get_y()
 
         # Step 4: Split data for training, validation, and testing.
-        x_train, x_test, y_train, y_test = self.split_data(x, y, self.test_percentage)
-        x_train, x_validate, y_train, y_validate = self.split_data(x_train, y_train, self.validate_percentage)
-
-        return x_train, y_train, x_validate, y_validate, x_test, y_test
-
-
-class MovielensCTRPredictionPreprocessor(BasePreprocessor):
-    """ Preprocess the Movielens 1M dataset for CTR prediction.
-
-    Note:
-        To obtain the Movielens 1M dataset, visit: https://grouplens.org/datasets/movielens/
-        The Movielens 1M dataset has 4 data columns: UserID, MovieID, Rating, and Timestamp.
-
-    Args:
-        non_csv_path (str): Path to convert the dataset into CSV format.
-        csv_path (str): Path to save and load the CSV dataset.
-        header (int): Row number to use as column names.
-        columns (list): String names associated with the columns of the dataset.
-        delimiter (str): Separator used to parse lines.
-        filler (float): Filler value used to fill missing data.
-        dtype_dict (dict): Map string column names to column data type.
-        ignored_columns (list): String names associated with the columns to ignore.
-        target_column (str): String name associated with the columns containing target data for prediction, e.g.,
-            rating column for rating prediction and label column for click-through rate (CTR) prediction.
-        numerical_columns (list): String names associated with the columns containing numerical data.
-        categorical_columns (list): String names associated with the columns containing categorical data.
-        categorical_filter (int): Filter used to group infrequent categories in one column as the same category.
-        fit_dictionary_path (str): Path to the fit dictionary for categorical data.
-        transform_path (str): Path to the transformed dataset.
-        test_percentage (float): Percentage for the test set.
-        validate_percentage (float): Percentage for the validation set.
-        train_path (str): Path to the training set.
-        validate_path (str): Path to the validation set.
-        test_path (str): Path to the test set.
-    """
-
-    # rp_neumf = 0.7677645683288574
-
-    def __init__(self,
-                 non_csv_path=None,
-                 csv_path='/home/thwang1231/autorec/examples/datasets/ml-1m/ratings.dat',
-                 header=None,  # inferred in load_data()
-                 columns=None,
-                 delimiter='::',
-                 filler=0.0,
-                 dtype_dict=None,
-                 ignored_columns=None,
-                 target_column='Rating',
-                 numerical_columns=None,
-                 categorical_columns=None,
-                 categorical_filter=0,  # no grouping, simply renumber
-                 fit_dictionary_path='/home/thwang1231/autorec/examples/preprocessed/movielens/fit_dictionary.pkl',
-                 transform_path='/home/thwang1231/autorec/examples/preprocessed/movielens/transformed.pkl',
-                 test_percentage=0.1,
-                 validate_percentage=0.1,
-                 train_path='/home/thwang1231/autorec/examples/preprocessed/movielens/train.pkl',
-                 validate_path='/home/thwang1231/autorec/examples/preprocessed/movielens/validate.pkl',
-                 test_path='/home/thwang1231/autorec/examples/preprocessed/movielens/test.pkl'):
-
-        if columns is None:
-            columns = ['UserID', 'MovieID', 'Rating', 'Timestamp']
-        if dtype_dict is None:
-            dtype_dict = {'UserID': np.int32, 'MovieID': np.int32, 'Rating': np.int32, 'Timestamp': np.int32}
-        if ignored_columns is None:
-            ignored_columns = columns[3]
-        if numerical_columns is None:
-            numerical_columns = []
-        if categorical_columns is None:
-            categorical_columns = columns[:2]
-
-        super().__init__(non_csv_path=non_csv_path,
-                         csv_path=csv_path,
-                         header=header,
-                         delimiter=delimiter,
-                         filler=filler,
-                         dtype_dict=dtype_dict,
-                         columns=columns,
-                         ignored_columns=ignored_columns,
-                         target_column=target_column,
-                         numerical_columns=numerical_columns,
-                         categorical_columns=categorical_columns,
-                         categorical_filter=categorical_filter,
-                         fit_dictionary_path=fit_dictionary_path,
-                         transform_path=transform_path,
-                         test_percentage=test_percentage,
-                         validate_percentage=validate_percentage,
-                         train_path=train_path,
-                         validate_path=validate_path,
-                         test_path=test_path)
-
-    def preprocess(self):
-        # Step 1: Load data for fit and transform categorical data.
-        self.load_dataset()
-
-        # Step 2: Transform categorical data.
-        self.transform_categorical()
-
-        # Step 3: Obtain LHS (X) and RHS (y) of the equation.
-        x = self.get_x()
-        y = self.get_y()
-
-        # Step 4: Split data for training, validation, and testing.
-        x_train, x_test, y_train, y_test = self.split_data(x, y, self.test_percentage)
-        x_train, x_validate, y_train, y_validate = self.split_data(x_train, y_train, self.validate_percentage)
-
-        # Step 5: Conduct negative sampling.
         x_train, x_test, y_train, y_test = self.split_data(x, y, self.test_percentage)
         x_train, x_validate, y_train, y_validate = self.split_data(x_train, y_train, self.validate_percentage)
 
