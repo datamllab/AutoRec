6f36f66abe16d10f2614564f5d66a0750dd38c21
eightants <yihonganthony@gmail.com>
2021-01-02 11:00:11 -0800

    Added dummy class to test base preprocessor functions

--- tests/pipeline_tests/test_preprocessor.py
+++ tests/pipeline_tests/test_preprocessor.py
@@ -12,13 +12,79 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 
-from autorecsys.pipeline.preprocessor import NetflixPrizePreprocessor, CriteoPreprocessor, AvazuPreprocessor, MovielensPreprocessor
+from autorecsys.pipeline.preprocessor import BasePreprocessor, NetflixPrizePreprocessor, CriteoPreprocessor, AvazuPreprocessor, MovielensPreprocessor
 
 
 logger = logging.getLogger(__name__)
 
 current_directory = os.path.dirname(os.path.abspath(__file__)) # directory of this test file so that datasets are imported no mattter where the code is run
 dataset_directory = os.path.join(current_directory,'../../examples/example_datasets')
+
+class DummyPreprocessor(BasePreprocessor):
+    """ Dummy class for testing base functions """
+
+    def __init__(self,
+                data_df=None,
+                 non_csv_path=None,
+                 csv_path=None,
+                 header=0, 
+                 columns=None,
+                 delimiter='\t',
+                 filler=0.0,
+                 dtype_dict=None,  # inferred in load_data()
+                 ignored_columns=None,
+                 target_column='rating',
+                 numerical_columns=None,
+                 categorical_columns=None,
+                 categorical_filter=0, # all categories are counted
+                 fit_dictionary_path=None,
+                 transform_path=None,
+                 test_percentage=0.1,
+                 validate_percentage=0.1,
+                 train_path=None,
+                 validate_path=None,
+                 test_path=None):
+
+        if columns is None:
+            columns = range(3)
+        if dtype_dict is None:
+            dtype_dict = {}
+        if ignored_columns is None:
+            ignored_columns = []
+        if numerical_columns is None:
+            numerical_columns = ['num_people']
+        if categorical_columns is None:
+            categorical_columns = ['user_id']
+
+        super().__init__(non_csv_path=non_csv_path,
+                         csv_path=csv_path,
+                         header=header,
+                         delimiter=delimiter,
+                         filler=filler,
+                         dtype_dict=dtype_dict,
+                         columns=columns,
+                         ignored_columns=ignored_columns,
+                         target_column=target_column,
+                         numerical_columns=numerical_columns,
+                         categorical_columns=categorical_columns,
+                         categorical_filter=categorical_filter,
+                         fit_dictionary_path=fit_dictionary_path,
+                         transform_path=transform_path,
+                         test_percentage=test_percentage,
+                         validate_percentage=validate_percentage,
+                         train_path=train_path,
+                         validate_path=validate_path,
+                         test_path=test_path)
+        self.data_df = data_df
+
+    def preprocess(self):
+        self.transform_categorical()
+        self.transform_numerical()
+        x = self.get_x()
+        y = self.get_y()
+        x_train, x_test, y_train, y_test = self.split_data(x, y, self.test_percentage)
+        x_train, x_validate, y_train, y_validate = self.split_data(x_train, y_train, self.validate_percentage)
+        return x_train, y_train, x_validate, y_validate, x_test, y_test
 
 
 class TestPreprocessors(unittest.TestCase):
@@ -30,14 +96,34 @@ class TestPreprocessors(unittest.TestCase):
     def setUp(self):
         super(TestPreprocessors, self).setUp()
 
-        column_names = ["user_id", "item_id", "rating"]
+        column_names = ["user_id", "num_people", "rating"]
         tabular_data = np.array([
             [1, 1, 1], [1, 2, 1], [1, 3, 1], [1, 4, 1],
             [2, 1, 1], [2, 2, 1], [2, 3, 1],
             [3, 1, 1], [3, 2, 1],
             [4, 1, 1]
         ])
+        self.input_size = 10
         self.input_df = pd.DataFrame(tabular_data, columns=column_names)
+    
+    def test_BasePreprocessor(self):
+        base = DummyPreprocessor(data_df=self.input_df)
+        base.transform_numerical()
+        assert base.data_df.shape == (10, 3)
+        base.transform_categorical()
+        assert base.data_df.shape == (10, 3)
+        assert base.get_hash_size() == [4]
+
+        x_cols = base.get_x()
+        x_sol = self.input_df.drop(["rating"], axis=1)
+        pd.testing.assert_frame_equal(x_sol, x_cols)
+        assert np.array_equal(base.get_x_numerical(x_cols), x_sol[['num_people']].values)
+        assert np.array_equal(base.get_x_categorical(x_cols), x_sol[['user_id']].values)
+
+        y_vals_sol = np.ones(self.input_size)
+        assert all(base.get_y() == y_vals_sol)
+        assert base.get_numerical_count() == 1
+        assert base.get_categorical_count() == 1
 
     def test_MovielensPreprocessor(self):
         movielens = MovielensPreprocessor(csv_path=os.path.join(dataset_directory,'movielens/ratings-10k.dat'))
@@ -110,5 +196,3 @@ class TestPreprocessors(unittest.TestCase):
         assert avazu.get_x().shape[0] == avazu.get_y().shape[0]
         assert len(avazu.get_hash_size()) == avazu.get_categorical_count()
         assert avazu.get_numerical_count() + avazu.get_categorical_count() == len(avazu.get_x().columns)
-
-
