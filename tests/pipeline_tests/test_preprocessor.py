d898b96e596eeb5c2439121c7905374c249c8f9e
eightants <yihonganthony@gmail.com>
2021-01-02 19:03:19 -0800

    Formatted code

--- tests/pipeline_tests/test_preprocessor.py
+++ tests/pipeline_tests/test_preprocessor.py
@@ -18,17 +18,20 @@ from autorecsys.pipeline.preprocessor import BasePreprocessor, NetflixPrizePrepr
 
 logger = logging.getLogger(__name__)
 
-current_directory = os.path.dirname(os.path.abspath(__file__)) # directory of this test file so that datasets are imported no mattter where the code is run
-dataset_directory = os.path.join(current_directory,'../../examples/example_datasets')
+# directory of this test file so that datasets are imported no mattter where the code is run
+current_directory = os.path.dirname(os.path.abspath(__file__))
+dataset_directory = os.path.join(
+    current_directory, '../../examples/example_datasets')
+
 
 class DummyPreprocessor(BasePreprocessor):
     """ Dummy class for testing base functions """
 
     def __init__(self,
-                data_df=None,
+                 data_df=None,
                  non_csv_path=None,
                  csv_path=None,
-                 header=0, 
+                 header=0,
                  columns=None,
                  delimiter='\t',
                  filler=0.0,
@@ -37,7 +40,7 @@ class DummyPreprocessor(BasePreprocessor):
                  target_column='rating',
                  numerical_columns=None,
                  categorical_columns=None,
-                 categorical_filter=0, # all categories are counted
+                 categorical_filter=0,  # all categories are counted
                  fit_dictionary_path=None,
                  transform_path=None,
                  test_percentage=0.1,
@@ -83,8 +86,10 @@ class DummyPreprocessor(BasePreprocessor):
         self.transform_numerical()
         x = self.get_x()
         y = self.get_y()
-        x_train, x_test, y_train, y_test = self.split_data(x, y, self.test_percentage)
-        x_train, x_validate, y_train, y_validate = self.split_data(x_train, y_train, self.validate_percentage)
+        x_train, x_test, y_train, y_test = self.split_data(
+            x, y, self.test_percentage)
+        x_train, x_validate, y_train, y_validate = self.split_data(
+            x_train, y_train, self.validate_percentage)
         return x_train, y_train, x_validate, y_validate, x_test, y_test
 
 
@@ -107,7 +112,7 @@ class TestPreprocessors(unittest.TestCase):
         self.validation_and_test_size = 0.1
         self.input_size = 10
         self.input_df = pd.DataFrame(tabular_data, columns=column_names)
-    
+
     def test_BasePreprocessor(self):
         base = DummyPreprocessor(data_df=self.input_df)
         base.transform_numerical()
@@ -119,8 +124,10 @@ class TestPreprocessors(unittest.TestCase):
         x_cols = base.get_x()
         x_sol = self.input_df.drop(["rating"], axis=1)
         pd.testing.assert_frame_equal(x_sol, x_cols)
-        assert np.array_equal(base.get_x_numerical(x_cols), x_sol[['num_people']].values)
-        assert np.array_equal(base.get_x_categorical(x_cols), x_sol[['user_id']].values)
+        assert np.array_equal(base.get_x_numerical(
+            x_cols), x_sol[['num_people']].values)
+        assert np.array_equal(base.get_x_categorical(
+            x_cols), x_sol[['user_id']].values)
 
         y_vals_sol = np.ones(self.input_size)
         assert all(base.get_y() == y_vals_sol)
@@ -128,7 +135,8 @@ class TestPreprocessors(unittest.TestCase):
         assert base.get_categorical_count() == 1
 
     def test_MovielensPreprocessor(self):
-        movielens = MovielensPreprocessor(csv_path=os.path.join(dataset_directory,'movielens/ratings-10k.dat'))
+        movielens = MovielensPreprocessor(csv_path=os.path.join(
+            dataset_directory, 'movielens/ratings-10k.dat'))
         train_X, train_y, val_X, val_y, test_X, test_y = movielens.preprocess()
         num_rows = 10000
         test_size = math.ceil(num_rows * self.validation_and_test_size)
@@ -138,15 +146,20 @@ class TestPreprocessors(unittest.TestCase):
         assert val_y.shape[0] == test_size
         assert test_X.shape[0] == test_size
         assert test_y.shape[0] == test_size
-        assert movielens.data_df.shape == (10000, 3) # check shape to verify transform functions
+        # check shape to verify transform functions
+        assert movielens.data_df.shape == (10000, 3)
         assert movielens.get_categorical_count() == 2
         assert movielens.get_numerical_count() == 0
-        assert movielens.get_x().shape[0] == movielens.get_y().shape[0] # check x and y have same length
-        assert len(movielens.get_hash_size()) == movielens.get_categorical_count()
-        assert movielens.get_numerical_count() + movielens.get_categorical_count() == len(movielens.get_x().columns) # check numerical + categorical = total columns
+        assert movielens.get_x().shape[0] == movielens.get_y(
+        ).shape[0]  # check x and y have same length
+        assert len(movielens.get_hash_size()
+                   ) == movielens.get_categorical_count()
+        assert movielens.get_numerical_count() + movielens.get_categorical_count() == len(
+            movielens.get_x().columns)  # check numerical + categorical = total columns
 
     def test_CriteoPreprocessor(self):
-        criteo = CriteoPreprocessor(csv_path=os.path.join(dataset_directory,'criteo/train-10k.txt'))
+        criteo = CriteoPreprocessor(csv_path=os.path.join(
+            dataset_directory, 'criteo/train-10k.txt'))
         train_X, train_y, val_X, val_y, test_X, test_y = criteo.preprocess()
         num_rows = 10000
         test_size = math.ceil(num_rows * self.validation_and_test_size)
@@ -161,10 +174,12 @@ class TestPreprocessors(unittest.TestCase):
         assert criteo.get_numerical_count() == 13
         assert criteo.get_x().shape[0] == criteo.get_y().shape[0]
         assert len(criteo.get_hash_size()) == criteo.get_categorical_count()
-        assert criteo.get_numerical_count() + criteo.get_categorical_count() == len(criteo.get_x().columns)
+        assert criteo.get_numerical_count(
+        ) + criteo.get_categorical_count() == len(criteo.get_x().columns)
 
     def test_NetflixPreprocessor(self):
-        netflix = NetflixPrizePreprocessor(non_csv_path=os.path.join(dataset_directory,'netflix/combined_data_1-10k.txt'), csv_path=os.path.join(dataset_directory,'netflix/combined_data_1-10k.csv'))
+        netflix = NetflixPrizePreprocessor(non_csv_path=os.path.join(
+            dataset_directory, 'netflix/combined_data_1-10k.txt'), csv_path=os.path.join(dataset_directory, 'netflix/combined_data_1-10k.csv'))
         train_X, train_y, val_X, val_y, test_X, test_y = netflix.preprocess()
         num_rows = 10000
         test_size = math.ceil(num_rows * self.validation_and_test_size)
@@ -179,10 +194,12 @@ class TestPreprocessors(unittest.TestCase):
         assert netflix.get_numerical_count() == 0
         assert netflix.get_x().shape[0] == netflix.get_y().shape[0]
         assert len(netflix.get_hash_size()) == netflix.get_categorical_count()
-        assert netflix.get_numerical_count() + netflix.get_categorical_count() == len(netflix.get_x().columns)
+        assert netflix.get_numerical_count(
+        ) + netflix.get_categorical_count() == len(netflix.get_x().columns)
 
     def test_AvazuPreprocessor(self):
-        avazu = AvazuPreprocessor(csv_path=os.path.join(dataset_directory,'avazu/train-10k'))
+        avazu = AvazuPreprocessor(csv_path=os.path.join(
+            dataset_directory, 'avazu/train-10k'))
         train_X, train_y, val_X, val_y, test_X, test_y = avazu.preprocess()
         num_rows = 9999
         test_size = math.ceil(num_rows * self.validation_and_test_size)
@@ -197,4 +214,5 @@ class TestPreprocessors(unittest.TestCase):
         assert avazu.get_numerical_count() == 0
         assert avazu.get_x().shape[0] == avazu.get_y().shape[0]
         assert len(avazu.get_hash_size()) == avazu.get_categorical_count()
-        assert avazu.get_numerical_count() + avazu.get_categorical_count() == len(avazu.get_x().columns)
+        assert avazu.get_numerical_count(
+        ) + avazu.get_categorical_count() == len(avazu.get_x().columns)
